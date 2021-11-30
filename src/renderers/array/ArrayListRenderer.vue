8a9c7077a998099b4702816cf03a728b124a7482
Maurier <mauriel.ramirez@gmail.com>
2021-11-29 17:36:02 -0700

    adding drag and drop

--- src/renderers/array/ArrayListRenderer.vue
+++ src/renderers/array/ArrayListRenderer.vue
@@ -8,29 +8,39 @@
       </md-button>
     </div>
 
-    <transition-group name="list-complete" class="list-elements-container" tag="div">
-      <array-list-element
-        v-for="(element, index) in control.data"
-        class="list-complete-item"
-        :key="`${control.path}-${index}`"
-        :styles="styles"
-        :moveUp="moveUp(control.path, index)"
-        :moveDown="moveDown(control.path, index)"
-        :delete="removeItems(control.path, [index])"
-        :moveUpEnabled="index > 0"
-        :moveDownEnabled="index < control.data.length - 1"
-        :label="childLabelForIndex(index)"
-      >
-        <dispatch-renderer
-          :schema="control.schema"
-          :uischema="childUiSchema"
-          :path="composePaths(control.path, `${index}`)"
-          :enabled="control.enabled"
-          :renderers="control.renderers"
-          :cells="control.cells"
-        />
-      </array-list-element>
-    </transition-group>
+    <drop-list name="list-complete" class="list-elements-container" 
+      :items="control.data || []"
+      @reorder="reorderElements($event.from, $event.to)"
+    >
+      <template v-slot:item="{item, index}">
+        <drag class="item" :key="index">
+          <array-list-element
+            class="list-complete-item"
+            :styles="styles"
+            :moveUp="moveUp(control.path, index)"
+            :moveDown="moveDown(control.path, index)"
+            :delete="removeItems(control.path, [index])"
+            :moveUpEnabled="index > 0"
+            :moveDownEnabled="index < control.data.length - 1"
+            :label="childLabelForIndex(index)"
+          >
+            <dispatch-renderer
+              :schema="control.schema"
+              :uischema="childUiSchema"
+              :path="composePaths(control.path, `${index}`)"
+              :enabled="control.enabled"
+              :renderers="control.renderers"
+              :cells="control.cells"
+            />
+          </array-list-element>
+        </drag>
+      </template>
+
+      <template v-slot:feedback="{data}">
+        <div class="item feedback" :key="data">{{data}}</div>
+      </template>
+      
+    </drop-list>
 
     <div v-if="noData" class="md-caption">
       Click on the '+' button above to add items.
@@ -56,12 +66,14 @@ import {
 } from '@jsonforms/vue2'
 import { useVanillaArrayControl } from "@jsonforms/vue2-vanilla"
 import ArrayListElement from './ArrayListElement.vue'
+import { Drag, Drop, DropList } from 'vue-easy-dnd'
 
 const controlRenderer = defineComponent({
   name: 'array-list-renderer',
   components: {
     ArrayListElement,
-    DispatchRenderer
+    DispatchRenderer,
+    Drop, DropList, Drag
   },
   props: {
     ...rendererProps<ControlElement>()
@@ -74,6 +86,8 @@ const controlRenderer = defineComponent({
       return !this.control.data || this.control.data.length === 0
     }
   },
+  // created() {
+  // },
   methods: {
     composePaths,
     createDefaultValue,
@@ -82,6 +96,12 @@ const controlRenderer = defineComponent({
         this.control.path,
         createDefaultValue(this.control.schema)
       )()
+    },
+    reorderElements(index, newIndex) {
+      if (newIndex < 0 || newIndex >= this.control.data.length) {
+          return
+      } // Already at the top or bottom.
+      this.control.data.splice(newIndex, 0, this.control.data.splice(index, 1)[0])
     }
   }
 })
@@ -104,6 +124,10 @@ export const arrayListRenderer: JsonFormsRendererRegistryEntry = {
       margin: 0;
     }
   }
+  
+  .md-subheading {
+    color: rgba(0, 0, 0, 0.54);
+  }
 
   .list-complete-item {
     transition: all 0.35s ease;
@@ -112,14 +136,9 @@ export const arrayListRenderer: JsonFormsRendererRegistryEntry = {
   .list-complete-enter,
   .list-complete-leave-to {
     opacity: 0;
-    // transform: translateY(30px);
   }
 
   .list-complete-leave-active {
-    position: absolute;
-  }
-
-  .md-subheading {
-    color: rgba(0, 0, 0, 0.54);
+    // position: absolute;
   }
 </style>
