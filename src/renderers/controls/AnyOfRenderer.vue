26c24b4fd45fc57672fd2718bfdbe8a02cb7a58e
Maurier <mauriel.ramirez@gmail.com>
2022-06-15 13:45:59 -0600

    fix parsing of description

--- src/renderers/controls/AnyOfRenderer.vue
+++ src/renderers/controls/AnyOfRenderer.vue
@@ -119,7 +119,7 @@
         </template>
       </template>
     </fieldset>
-    <div v-if="control.schema.options.description" class="text--secondary text-body-1 ml-2">{{ control.schema.options.description }}</div>
+    <div v-if="control.schema.description" class="text--secondary text-body-1 ml-2">{{ control.schema.description }}</div>
     <div v-if="control.errors" class="ml-2 v-messages error--text" :class="styles.control.error">
       <v-divider v-if="isFlat" class="mb-4"></v-divider>
       {{ control.errors }}
