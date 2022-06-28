c25141211b9e2b4e60731f28e82f323603a1cdb8
Maurier <mauriel.ramirez@gmail.com>
2022-06-28 09:47:04 -0600

    update help email

--- src/components/contact/cz.contact.vue
+++ src/components/contact/cz.contact.vue
@@ -8,13 +8,13 @@
     <div class="text-h6 has-space-bottom">Join Us in the CZNet Slack Workspace</div>
 
     <p class="text-body-1">
-      The CZNet Slack workspace was established to foster communication between the thematic clusters and with the CZNet Hub team. If you haven’t joined the CZNet Slack workspace, we encourage you to join us there. There are specific channels set up within the workspace for topical discussions. If you would like to join the CZNet Slack workspace, please email us at <a href="mailto:cznet@cuahsi.org">cznet@cuahsi.org</a>.
+      The CZNet Slack workspace was established to foster communication between the thematic clusters and with the CZNet Hub team. If you haven’t joined the CZNet Slack workspace, we encourage you to join us there. There are specific channels set up within the workspace for topical discussions. If you would like to join the CZNet Slack workspace, please email us at <a href="mailto:help@cuahsi.org">help@cuahsi.org</a>.
     </p>
 
     <div class="text-h6 has-space-bottom">Contact Us Directly via Email</div>
     
     <p class="text-body-1">
-      For specific questions about CZNet data management, please contact us at <a href="mailto:cznet@cuahsi.org?subject=(CZNet)">cznet@cuahsi.org</a>. Be sure to include “CZNet” in the subject line of the email you send. We will route these emails to the person(s) who we think can best answer your questions.
+      For specific questions about CZNet data management, please contact us at <a href="mailto:help@cuahsi.org?subject=(CZNet)">help@cuahsi.org</a>. Be sure to include “CZNet” in the subject line of the email you send. We will route these emails to the person(s) who we think can best answer your questions.
     </p>
   </v-container>
 </template>
