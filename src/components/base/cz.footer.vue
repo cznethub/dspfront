6d12f646952746f2ecdb755d31ce0c59434dc2ea
Maurier <mauriel.ramirez@gmail.com>
2022-07-12 12:22:22 -0600

    update year in footer

--- src/components/base/cz.footer.vue
+++ src/components/base/cz.footer.vue
@@ -26,7 +26,7 @@
     <v-divider></v-divider>
 
     <div class="text-center d-flex flex-column align-center has-space-top-2x">
-      <p>(c) 2021 CUAHSI. ﻿This material is based upon work supported by the National Science Foundation (NSF) under awards 2012893, 2012593, and 2012748.<br>
+      <p>(c) {{year}} CUAHSI. ﻿This material is based upon work supported by the National Science Foundation (NSF) under awards 2012893, 2012593, and 2012748.<br>
       Any opinions, findings, conclusions, or recommendations expressed in this material are those of the authors and do not necessarily reflect the views of the NSF.</p>
     </div>
   </v-container>
@@ -51,6 +51,10 @@
 
     protected get version() {
       return process.env.VUE_APP_VERSION || '0'
+    }
+
+    protected get year() {
+      return (new Date()).getFullYear()
     }
   }
 </script>
