899bcd5aa57a5aafde96c420c5907379ff3e9778
Devin Cowan <dcowan@cuahsi.org>
2023-05-24 13:45:51 -0400

    add cypress and modify ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vuetify({ autoImport: true }),
+    vue(), 
+    vuetify({ autoImport: true })
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   define: {
     'process.env': {}
