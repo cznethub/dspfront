f59ba3e4717d46be72909aeb8959373e57fad914
Scott Black <sblack@cuahsi.org>
2024-07-08 14:10:30 -0600

    js folder changed with node upgrade

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ ROOT_DIR=/dspfront
 
 # Replace env vars in JavaScript files
 echo "Replacing env constants in JS"
-for file in $ROOT_DIR/js/app.*.js* $ROOT_DIR/index.html;
+for file in $ROOT_DIR/assets/index*.js $ROOT_DIR/index.html;
 do
   echo "Processing $file ...";
 
