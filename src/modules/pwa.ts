99bbe5abc2d0a9f463eb969aad75b718ce126138
Maurier <mauriel.ramirez@gmail.com>
2024-08-22 12:32:49 -0600

    disable vitePWA

--- src/modules/pwa.ts
+++ src/modules/pwa.ts
@@ -1,14 +1,14 @@
-import type { UserModule } from '~/types'
+// import type { UserModule } from '~/types'
 
-// https://github.com/antfu/vite-plugin-pwa#automatic-reload-when-new-content-available
-export const install: UserModule = ({ isClient, router }) => {
-  if (!isClient)
-    return
+// // https://github.com/antfu/vite-plugin-pwa#automatic-reload-when-new-content-available
+// export const install: UserModule = ({ isClient, router }) => {
+//   if (!isClient)
+//     return
 
-  router.isReady()
-    .then(async () => {
-      const { registerSW } = await import('virtual:pwa-register')
-      registerSW({ immediate: true })
-    })
-    .catch(() => {})
-}
+//   router.isReady()
+//     .then(async () => {
+//       const { registerSW } = await import('virtual:pwa-register')
+//       registerSW({ immediate: true })
+//     })
+//     .catch(() => {})
+// }
