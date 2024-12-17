8b941ea1df0509e59774ff3c2ead984139c7f3ea
Maurier <mauriel.ramirez@gmail.com>
2024-12-17 08:24:31 -0700

    initial configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'cypress'
-import vitePreprocessor from 'cypress-vite'
+import { defineConfig } from "cypress";
+import vitePreprocessor from "cypress-vite";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:8080',
+    baseUrl: "https://127.0.0.1",
     chromeWebSecurity: false,
-    specPattern: 'cypress/e2e/**/*.spec.*',
+    specPattern: "cypress/e2e/**/*.spec.*",
     supportFile: false,
     setupNodeEvents(on) {
-      on('file:preprocessor', vitePreprocessor())
+      on("file:preprocessor", vitePreprocessor());
     },
   },
   component: {
     devServer: {
-      framework: 'vue-cli',
-      bundler: 'webpack',
+      framework: "vue-cli",
+      bundler: "webpack",
     },
   },
-})
+});
